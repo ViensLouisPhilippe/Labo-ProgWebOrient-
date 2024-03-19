2c3f0e513db60e999c492703379e1023c285b371
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2024-03-19 14:26:48 -0400

    Labo8 partie 1

---
+++ labo8_v2/labo8_v2/Labo8api_(vide)/LaboSemaine8/Data/LaboSemaine8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LaboSemaine8.Models;
+
+namespace LaboSemaine8.Data
+{
+    public class LaboSemaine8Context : DbContext
+    {
+        public LaboSemaine8Context (DbContextOptions<LaboSemaine8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LaboSemaine8.Models.Animal> Animal { get; set; } = default!;
+    }
+}
---
+++ labo8_v2/labo8_v2/Labo8api_(vide)/LaboSemaine8/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace LaboSemaine8.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
